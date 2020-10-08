9e8d9d4fe8cb54a61ac08266d8ff6813d49bb3d8
hsshin <shinhash123@gmail.com>
2020-10-08 18:13:17 +0900

    python thread test commit wrtier : shinhash

---
+++ day11/MyThread.py
@@ -0,0 +1,15 @@
+import threading, time
+
+def sum(low, high):
+    total = 0
+    for i in range(low, high):
+        total += 1
+        time.sleep(1)
+        print("subThread", total)
+        
+    
+if __name__ == "__main__":   
+    t = threading.Thread(target=sum, args=(1, 20))
+    t.start()
+    
+    print("main Thread")    
